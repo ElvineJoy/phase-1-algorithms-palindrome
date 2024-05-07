7caf78225073163d7a0ebcca5c113d55072aa329
ElvineJoy <elvinkuiyungest@gmail.com>
2024-05-07 15:29:25 +0300

    Is palindrome lab completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reversedWord = word.split('').reverse().join('');
+  if (word === reversedWord){
+    return true;
+  } else 
+    return false;
+};
 
+isPalindrome("JavaScript");
+isPalindrome("a");
 /* 
   Add your pseudocode here
 */
@@ -9,6 +16,13 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+`The function takes in one argument, in this case word, and then using the inbult methods, we assign a new 
+variable to the argumnet and apply these methods on it to compare them to get a result by inputing conditionals 
+using if.
+split(), which splits the string into an array of substrings, returning a new array. On the new array, we 
+use the reverse() method (this is done because a palindrome read whether from left to righ or right to left
+should read the same way). Lastly we use the join to rejoin the sustrings into a string, which we shall then compare
+to the original word and see if they match.`
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
